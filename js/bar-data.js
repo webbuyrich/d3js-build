493fa37f5bf520a6fdc3971b38b62ea225c8a7c0
rpeterson <webbuyrich@gmail.com>
2019-03-27 00:42:21 -0700

    Added colors to bar chart

--- js/bar-data.js
+++ js/bar-data.js
@@ -1,6 +1,6 @@
-var bardata = [12, 60, 45, 15];
+var bardata = [12, 60, 85, 15,23, 65, 70, 10, 19, 30, 80];
 var height = 400,
-	width = 600,
+	width = 900,
 	barWidth = 100,
 	barOffset = 5;
 
@@ -9,7 +9,16 @@ var yScale = d3.scale.linear()
 	.domain([0, d3.max(bardata)])
 	.range([0, height]);
 
-	
+// use ordinal scale
+var xScale = d3.scale.ordinal()
+	.domain(bardata)
+	.rangeBands([0, width], .1, .2);
+
+// use d3 color scheme
+var colors = d3.scale.category20c()
+	.domain([0, bardata.length]);
+
+console.log(colors);
 // start d3 canvas
 d3.select('#graph')
 	.append('svg')
@@ -25,12 +34,12 @@ d3.select('#graph')
 	.data(bardata)
 	// start bars
 	.enter().append('rect')
-		.style('fill', '#2980b9')
+		.style('fill', function(d){return colors(d)})
 		.attr({
-			'width': barWidth,			
+			width: function(d) { return xScale.rangeBand()  } ,			
 			height: function(d){ return yScale(d) } ,
 			// return bar depending on data values
-			x: function(d, i){ return i *(barWidth + barOffset)},
+			x: function(d, i){ return xScale(d)},
 			y: function(d) { return height - yScale(d) },
 		})
 ;
