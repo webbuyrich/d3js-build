9607f2c4002266a0d74689ff73d0dc1150d4a659
rpeterson <webbuyrich@gmail.com>
2019-03-27 21:58:34 -0700

    Add opacity change on mouseover and mouseout

--- js/bar-data.js
+++ js/bar-data.js
@@ -1,5 +1,5 @@
 var bardata = [];
-for (i=0; i<50; i++){
+for (i=0; i<100; i++){
 	bardata.push(Math.random() * 6);
 }
 var height = 400,
@@ -22,7 +22,7 @@ var colors = d3.scale.linear()
 	.domain([0, bardata.length * .33, bardata.length * .66, bardata.length])
 	.range(['#fff','#1abc9c', '#2980b9', '#9b59b6', '#3498db']);
 
-console.log(colors);
+
 // start d3 canvas
 d3.select('#graph')
 	.append('svg')
@@ -46,4 +46,15 @@ d3.select('#graph')
 			x: function(d, i){ return xScale(d)},
 			y: function(d) { return height - yScale(d) },
 		})
+
+	// add opacity on mouseover
+	.on('mouseover', function(d){
+		d3.select(this)
+			.style('opacity', 0.5)
+	})	
+	// remove opacity on mouseout
+	.on('mouseout', function(d){
+		d3.select(this)
+			.style('opacity', 1)
+	})	
 ;
