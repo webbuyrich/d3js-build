51b968d03611349a2da2b7401a724a56720c621f
rpeterson <webbuyrich@gmail.com>
2019-03-28 00:38:55 -0700

    imported json file, created new variable lists, loop through data file

--- js/bar-data.js
+++ js/bar-data.js
@@ -1,100 +1,117 @@
-var bardata = [];
-for (i=0; i<50; i++){
-	bardata.push(Math.random() * 6);
-}
-var height = 400,
-	width = 900,
-	barWidth = 100,
-	barOffset = 5,
-	tempColor;
+// read data file - json import
+d3.json('data/forecast.json', function(d){
+
+	var temperatures = [],
+		height = 400,
+		width = 900,
+		barWidth = 100,
+		barOffset = 5;
+		
+	// no value variables
+	var tempColor,
+		yScale,
+		xScale,
+		colors,
+		tooltip,
+		myChart;
 
 
+	// loop through data file
+	for (i=0; i< d.list.length; i++){
+		temperatures.push(d.list[i].main.temp);
+	}
 
-// use linear scale
-var yScale = d3.scale.linear()
-	.domain([0, d3.max(bardata)])
-	.range([0, height]);
+	// use linear scale
+	yScale = d3.scale.linear()
+		.domain([0, d3.max(temperatures)])
+		.range([0, height]);
 
-// use ordinal scale
-var xScale = d3.scale.ordinal()
-	.domain(bardata)
-	.rangeBands([0, width], .1, .2);
+	// use ordinal scale
+	xScale = d3.scale.ordinal()
+		.domain(temperatures)
+		.rangeBands([0, width], .1, .2);
 
-// use d3 color scheme
-var colors = d3.scale.linear()
-	.domain([0, bardata.length * .33, bardata.length * .66, bardata.length])
-	.range(['#fff','#1abc9c', '#2980b9', '#9b59b6', '#3498db']);
-
-
-// create a tooltip
-var tooltip = d3.select('body')
-				.append('div')
-				.style({
-					'position':'absolute',
-					'padding': '0 10px',
-					'background': '#fff',
-					'opacity': '0.5'
-				})
+	// use d3 color scheme
+	colors = d3.scale.linear()
+		.domain([0, temperatures.length * .33, temperatures.length * .66, temperatures.length])
+		.range(['#fff','#1abc9c', '#2980b9', '#9b59b6', '#3498db']);
 
 
-// start d3 canvas
-var myChart = 
-	d3.select('#graph')
-		.append('svg')
-		.attr({
-			'width': width,
-			'height': height,
-
+	// create a tooltip
+	tooltip = d3.select('body')
+		.append('div')
+		.style({
+			'position':'absolute',
+			'padding': '0 10px',
+			'background': '#fff',
+			'opacity': '0.5'
 		})
-		.style('background', '#2c3e50')
-	// start graph
-	.selectAll('rect')
-		//get data
-		.data(bardata)
-		// start bars
-		.enter().append('rect')
-			.style('fill', function(d, i){return colors(i)})
+
+
+	// start d3 canvas
+	myChart = 
+		d3.select('#graph')
+			.append('svg')
 			.attr({
-				width: function(d) { return xScale.rangeBand()  } ,			
-				'height': 0 ,
-				// return bar depending on data values
-				x: function(d, i){ return xScale(d)},
-				'y': height
+				'width': width,
+				'height': height,
+
 			})
-
-		// add opacity on mouseover
-		.on('mouseover', function(d){
-			// tooltip transition
-			tooltip.transition().duration(200)
-				.style('opacity', 0.9)
-
-			// add tooltip 
-			tooltip.html(d)
-				.style({
-					'left': (d3.event.pageX - 35) + 'px',
-					'top': (d3.event.pageY - 130) + 'px'
+			.style('background', '#2c3e50')
+		// start graph
+		.selectAll('rect')
+			//get data
+			.data(temperatures)
+			// start bars
+			.enter().append('rect')
+				.style('fill', function(d, i){return colors(i)})
+				.attr({
+					width: function(d) { return xScale.rangeBand()  } ,			
+					'height': 0 ,
+					// return bar depending on data values
+					x: function(d, i){ return xScale(d)},
+					'y': height
 				})
-			tempColor = this.style.fill;
-			d3.select(this)
-				.style({
-					'fill': '#e67e22'
-				})
-		})	
-		// remove opacity on mouseout
-		.on('mouseout', function(d){
-			d3.select(this)
-				.style({
-					'fill': tempColor
-				})
-		});
 
-myChart.transition()
-	.attr({
-		height: function(d){ return yScale(d) } ,
-		y: function(d) { return height - yScale(d) },
-	})
-	.delay(function(d,i){
-		return i * 50;
-	})
-	.duration(1000)
+			// add opacity on mouseover
+			.on('mouseover', function(d){
+				// tooltip transition
+				tooltip.transition().duration(200)
+					.style('opacity', 0.9)
+
+				// add tooltip 
+				tooltip.html(d)
+					.style({
+						'left': (d3.event.pageX - 35) + 'px',
+						'top': (d3.event.pageY - 130) + 'px'
+					})
+				tempColor = this.style.fill;
+				d3.select(this)
+					.style({
+						'fill': '#e67e22'
+					})
+			})	
+			// remove opacity on mouseout
+			.on('mouseout', function(d){
+				d3.select(this)
+					.style({
+						'fill': tempColor
+					})
+			});
+
+	myChart.transition()
+		.attr({
+			height: function(d){ return yScale(d) } ,
+			y: function(d) { return height - yScale(d) },
+		})
+		.delay(function(d,i){
+			return i * 50;
+		})
+		.duration(1000)
+
+
+});
+
+
+
 
