24e13966d8f766d3b4e0bce5f77564ac9e09c516
rpeterson <webbuyrich@gmail.com>
2019-03-28 01:10:23 -0700

    add y guide with ticks to show range

--- js/bar-data.js
+++ js/bar-data.js
@@ -8,6 +8,8 @@ d3.json('data/forecast.json', function(d){
 	// no value variables
 	var tempColor,
 		yScale,
+		yAxisValues,
+		yAxisTicks,
 		xScale,
 		colors,
 		tooltip,
@@ -23,6 +25,20 @@ d3.json('data/forecast.json', function(d){
 	yScale = d3.scale.linear()
 		.domain([0, d3.max(temperatures)])
 		.range([0, height]);
+
+	// create yAxis values
+	yAxisValues = d3.scale.linear()
+		.domain([0, d3.max(temperatures)])
+		// reverse height to draw scale correctly
+		.range([height, 0]);
+
+	// create yAxis tick marks
+	yAxisTicks = d3.svg.axis()
+		.scale(yAxisValues)
+		//define how many ticks
+		.ticks(10)
+		// define which side to display
+		.orient('left');
 
 	// use ordinal scale
 	xScale = d3.scale.ordinal()
@@ -55,50 +71,57 @@ d3.json('data/forecast.json', function(d){
 				'height': height,
 
 			})
-			
-		// start graph
-		.selectAll('rect')
-			//get data
-			.data(temperatures)
-			// start bars
-			.enter().append('rect')
-				.style('fill', colors)
-				.attr({
-					width: function(d) { return xScale.rangeBand()  } ,			
-					'height': 0 ,
-					// return bar depending on data values
-					x: function(d, i){ return xScale(d)},
-					'y': height
-				})
+			// start to group the elements
+			.append('g')			
+			// start graph
+			.selectAll('rect')
+				//get data
+				.data(temperatures)
+				// start bars
+				.enter().append('rect')
+					.style('fill', colors)
+					.attr({
+						width: function(d) { return xScale.rangeBand()  } ,			
+						'height': 0 ,
+						// return bar depending on data values
+						x: function(d, i){ return xScale(d)},
+						'y': height
+					})
 
-			// add opacity on mouseover
-			.on('mouseover', function(d){
-				// tooltip transition
-				tooltip.transition().duration(200)
-					.style('opacity', 0.9)
+				// add opacity on mouseover
+				.on('mouseover', function(d){
+					// tooltip transition
+					tooltip.transition().duration(200)
+						.style('opacity', 0.9)
 
-				// add tooltip 
-				tooltip.html(
-					'<div style="font-size: 2rem; font-weight:bold">'+ d + '&deg;</div>'
-				)
-					.style({
-						'left': (d3.event.pageX - 35) + 'px',
-						'top': (d3.event.pageY - 30) + 'px'
-					})
-				tempColor = this.style.fill;
-				d3.select(this)
-					.style({
-						'fill': '#e67e22'
-					})
-			})	
-			// remove opacity on mouseout
-			.on('mouseout', function(d){
-				tooltip.html('')
-				d3.select(this)
-					.style({
-						'fill': tempColor
-					})
-			});
+					// add tooltip text
+					tooltip.html(
+						'<div style="font-size: 2rem; font-weight:bold">'+ d + '&deg;</div>'
+					)
+						.style({
+							'left': (d3.event.pageX - 35) + 'px',
+							'top': (d3.event.pageY - 30) + 'px'
+						})
+					tempColor = this.style.fill;
+					d3.select(this)
+						.style({
+							'fill': '#e67e22'
+						})
+				})	
+				// remove opacity on mouseout
+				.on('mouseout', function(d){
+					// set tooltip to empty once you mouseout
+					tooltip.html('')
+					d3.select(this)
+						.style({
+							'fill': tempColor
+						})
+				});
+
+	// create the y ticks 
+	yGuide = d3.select('#graph svg').append('g')
+			.attr('transform', 'translate(20,0)')
+			.call(yAxisTicks)
 
 	myChart.transition()
 		.attr({
